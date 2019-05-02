1d868e24e8d492dc8a171509974979b0745b3884
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-02 00:44:14 -0300

    configurando rota para vehicles-list e vehicles-form

--- src/app/pages/vehicles/vehicles-routing.module.ts
+++ src/app/pages/vehicles/vehicles-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VehiclesListComponent } from './vehicles-list/vehicles-list.component';
+import { VehiclesFormComponent } from './vehicles-form/vehicles-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: VehiclesListComponent },
+  { path: 'novo', component: VehiclesFormComponent },
+  { path: ':id/editar', component: VehiclesFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
