c1dad16ae396adf0321d0ca6edb11e242ded1866
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-29 21:08:15 -0300

    Removido imports desnecessários e importado módulo core-module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
-
+import { CoreModule } from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-// Import your library
-import { NgFloatingActionMenuModule } from 'ng-floating-action-menu';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    CoreModule,
     AppRoutingModule,
-    FormsModule,
-
-    // Specify your library as an import
-    NgFloatingActionMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
