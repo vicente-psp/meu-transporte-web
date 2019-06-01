c31c69d393fe6fdb253934f30395c955f8359558
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-31 21:23:57 -0300

    Criado rota para drivers.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'vehicles', loadChildren: './pages/vehicles/vehicles.module#VehiclesModule'}
+  { path: 'drivers', loadChildren: './pages/drivers/drivers.module#DriversModule'}
 ];
 
 @NgModule({
