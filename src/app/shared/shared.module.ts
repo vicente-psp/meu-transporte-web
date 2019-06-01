f6533b4c202a0f34523f709c5b30707371a41810
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-31 22:30:25 -0300

    Importado e exportado componentes do angular material e componentes do primeng.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,35 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
 import { ServerErrorMessagesComponent } from './components/server-error-messages/server-error-messages.component';
 
+// componentes do primeng
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
+import { CardModule } from 'primeng/card';
+
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
     RouterModule,
-    ButtonModule
+    ButtonModule,
+    InputTextModule,
+    PanelModule,
+    CardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
   ],
   declarations: [
     BreadCrumbComponent,
@@ -30,7 +46,13 @@ import { ButtonModule } from 'primeng/button';
     PageHeaderComponent,
     FormFieldErrorComponent,
     ServerErrorMessagesComponent,
-    ButtonModule
+    ButtonModule,
+    InputTextModule,
+    PanelModule,
+    CardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
   ]
 })
 export class SharedModule { }
