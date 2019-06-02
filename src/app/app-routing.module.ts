590c6cde85de72a8ad02901c61ac8889e31b8af5
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-02 18:55:11 -0300

    Alterado nome do caminho da rota drivers para motoristas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'drivers', loadChildren: './pages/drivers/drivers.module#DriversModule'}
+  { path: 'motoristas', loadChildren: './pages/drivers/drivers.module#DriversModule'}
 ];
 
 @NgModule({
