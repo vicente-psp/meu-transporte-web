329b03dd272b437fefba20d5bb20a47e33f5132c
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-03 21:40:31 -0300

    Criado recurso clients para consumir pelas requisições http.

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,11 +1,18 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Driver } from './pages/drivers/shared/driver.model';
+import { Client } from './pages/clients/shared/client.model';
  
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
     let drivers: Driver[] = [
       { id: 1, name: 'João Paulo', dateOfBirth: new Date(1992, 9, 24), cnh: '2353423', cpf: '12345632112', phone: '62984994081' },
     ];
-    return {drivers};
+
+
+    let clients: Client[] = [
+      { id: 1, name: 'Pedro', dateOfBirth: new Date(1990, 5, 4), cnh: '98782353423', cpf: '12345632112', phone: '62984496473' },
+    ];
+
+    return {drivers, clients};
   }
 }
