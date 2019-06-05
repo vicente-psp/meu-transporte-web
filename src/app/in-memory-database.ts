a24eb4de36f8dd836b239238228b88c5d658c701
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-04 23:20:21 -0300

    Criado recurso de produtos para consumir via http.

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,6 +1,7 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { Driver } from './pages/drivers/shared/driver.model';
 import { Client } from './pages/clients/shared/client.model';
+import { Product } from './pages/products/shared/product.model';
  
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
@@ -13,6 +14,11 @@ export class InMemoryDatabase implements InMemoryDbService {
       { id: 1, name: 'Pedro', dateOfBirth: new Date(1990, 5, 4), cnh: '98782353423', cpf: '12345632112', phone: '62984496473' },
     ];
 
-    return {drivers, clients};
+
+    let products: Product[] = [
+      { id: 1, name: 'Pneu', dateOfBirth: new Date(1990, 5, 4), cnh: '98782353423', cpf: '12345632112', phone: '62984496473' },
+    ];
+
+    return {drivers, clients, products};
   }
 }
