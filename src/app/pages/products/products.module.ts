7cbbbbe0921c43d66bd338b00007d2666e9dfd67
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-04 23:22:38 -0300

    Criado módulo e routing do recurso product.

---
+++ src/app/pages/products/products.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+
+import { IMaskModule } from 'angular-imask';
+
+import { SharedModule } from '../../shared/shared.module';
+
+import { ProductsRoutingModule } from './products-routing.module';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+import { MAT_DATE_LOCALE, MAT_DATE_FORMATS } from '@angular/material';
+
+export const DD_MM_YYYY_Format = {
+  parse: {
+      dateInput: 'LL',
+  },
+  display: {
+      dateInput: 'DD/MM/YYYY',
+      monthYearLabel: 'MMM YYYY',
+      dateA11yLabel: 'LL',
+      monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
+@NgModule({
+  imports: [
+    SharedModule,
+    ProductsRoutingModule,
+    IMaskModule,
+  ],
+  declarations: [
+    ProductFormComponent,
+    ProductListComponent
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    { provide: MAT_DATE_FORMATS, useValue: DD_MM_YYYY_Format }
+  ],
+})
+export class ProductsModule { }
