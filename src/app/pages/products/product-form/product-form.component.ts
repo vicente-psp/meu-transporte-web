c1d21ff85d46ea38d152ee777f571f179538a655
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-05 22:41:47 -0300

    Removido atributos e métodos não utilizados | modificado atributos da validação do recurso.

--- src/app/pages/products/product-form/product-form.component.ts
+++ src/app/pages/products/product-form/product-form.component.ts
@@ -12,51 +12,23 @@ import { ProductService } from '../shared/product.service';
 })
 export class ProductFormComponent extends BaseResourceFormComponent<Product> {
 
-  public phoneMask = {mask: '(00)00000-0000'};
-  public cpfMask = {mask: '000.000.000-00'};
-  public cnhMask = {mask: '00000000000'};
-  public dateMask = {mask: '00/00/0000'};
-
-  maxDate: Date = new Date();
 
   constructor(
     protected productService: ProductService,
     protected injector: Injector
   ) {
     super(injector, new Product(), productService, Product.fromJson);
-
-
-    this.maxDate = new Date(new Date().getFullYear() - 18, new Date().getMonth() + 1, new Date().getDate());
   }
 
+
   protected isValidResource(): boolean {
-    if (!this.isStringValid(this.resource.name)) {
+    if (!this.isStringValid(this.resource.description)) {
       return false;
     }
-    if (this.isDateValid(this.resource.dateOfBirth) && this.resource.dateOfBirth > this.maxDate) {
-      return false;
-    }
-    if (!this.isStringValid(this.resource.cnh)) {
-      return false;
-    }
-    if (!this.isStringValid(this.resource.cpf)) {
-      return false;
-    }
-    if (!this.isStringValid(this.resource.phone)) {
+    if (!this.isNumberValid(this.resource.unitValue)) {
       return false;
     }
     return true;
-  }
-
-  keyupClick(event): void {
-    console.log(event.key);
-  }
-  
-  convertToDate(event: any): void {
-    if(!this.isNullOrUndefined(event.srcElement.value)) {
-      const str = event.srcElement.value;
-      this.resource.dateOfBirth = this.convertDateUSFromStringBr(str.split("/"));
-    }
   }
 
 }
