e34f1537dcf24775f63224d3baa65abf17f42f62
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-09 18:26:50 -0300

    Inserido botão de filtro e modificados nomes das inputs de cadastro | feito padronização de valores default para os botões .

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
@@ -8,16 +8,42 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PageHeaderComponent implements OnInit {
 
   @Input('page-title') pageTitle: string;
-  @Input('show-button') showButton: boolean = true;
-  @Input('button-class') buttonClass: string;
-  @Input('button-color') buttonColor: string = 'primary';
-  @Input('button-text') buttonText: string;
-  @Input('button-link') buttonLink: string;
-  @Input('button-mat-icon') buttonMatIcon: string = 'add';
+  @Input('button-register-show') buttonRegisterShow: boolean = true;
+  @Input('button-register-color') buttonRegisterColor: string = 'primary';
+  @Input('button-register-text') buttonRegisterText: string = '';
+  @Input('button-register-tooltip') buttonRegisterTooltip: string = 'Cadastrar novo registro';
+  @Input('button-register-tooltip-position') buttonRegisterTooltipPosition: string = 'above';
+  @Input('button-register-link') buttonRegisterLink: string = 'cadastrar';
+  @Input('button-register-mat-icon') buttonRegisterMatIcon: string = 'add';
+
+
+  @Input('button-filter-show') buttonFilterShow: boolean = true;
+  @Input('button-filter-color') buttonFilterColor: string = 'accent';
+  @Input('button-filter-tooltip-position') buttonFilterTooltipPosition: string = 'above';
+  @Input('button-filter-text') buttonFilterText: string = '';
+  @Input('button-filter-mat-icon') buttonFilterMatIcon: string = 'filter_list';
+  @Output() buttonFilterEventEmitter: EventEmitter<any> = new EventEmitter<any>();
+  public buttonFilterTooltip: string = 'Exibir filtro';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  buttonFilterClick() {
+    if (this.buttonFilterTooltip === 'Exibir filtro') {
+      this.buttonFilterTooltip = 'Ocultar filtro';
+      this.buttonFilterMatIcon = 'close';
+      this.buttonFilterEventEmitter.emit(true);
+    } else {
+      this.buttonFilterTooltip = 'Exibir filtro';
+      this.buttonFilterMatIcon = 'filter_list';
+      this.buttonFilterEventEmitter.emit(false);
+    }
+  }
+
+  isShowButtons(): boolean {
+    return this.buttonRegisterShow || this.buttonFilterShow;
   }
 
 }
