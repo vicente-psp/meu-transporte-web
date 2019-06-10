8aad1e3c141b50955209dbf787cd940f48bd334d
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-09 23:56:12 -0300

    Comentado as rotas de clients e products enquanto é implementado corretamente o filtro e modificado as tabelas de listagens dos recursos.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'motoristas', loadChildren: './pages/drivers/drivers.module#DriversModule'},
-  { path: 'clientes', loadChildren: './pages/clients/clients.module#ClientsModule'},
-  { path: 'produtos', loadChildren: './pages/products/products.module#ProductsModule'},
+  // { path: 'clientes', loadChildren: './pages/clients/clients.module#ClientsModule'},
+  // { path: 'produtos', loadChildren: './pages/products/products.module#ProductsModule'},
 ];
 
 @NgModule({
