c385144db952f6e9f08f233e97b2699ca1f8798b
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-10 21:10:21 -0300

    Criado variáveis e método abstrato para manipular as buscas do filtro.

--- src/app/shared/components/base-resource-filter/base-resource-filter.component.ts
+++ src/app/shared/components/base-resource-filter/base-resource-filter.component.ts
@@ -1,20 +1,34 @@
-import { OnInit, Output, EventEmitter } from '@angular/core';
+import { OnInit, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { BaseResourceModel } from '../../models/base-resource.model';
 import { BaseResourceService } from '../../services/base-resource.service';
 import { isNullOrUndefined, isDate } from 'util';
+import { Subscription, Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
-export abstract class BaseResourceFilterComponent<T extends BaseResourceModel> implements OnInit {
+export abstract class BaseResourceFilterComponent<T extends BaseResourceModel> implements OnInit, OnDestroy {
 
   resources: MatTableDataSource<T> = new MatTableDataSource<T>();
+  subscription: Subscription;
+  subject: Subject<string> = new Subject<string>();
 
   @Output() changeResources: EventEmitter<Array<T>> = new EventEmitter<Array<T>>();
 
   constructor(private resourcesService: BaseResourceService<T>, public resource: T) { }
 
   ngOnInit() {
+    this.subscription = this.subject.pipe(debounceTime(1500)).subscribe(nextValue => {
+      this.resultFilter(nextValue);
+    });
   }
+
+  
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  abstract resultFilter(nameInput: string): void;
 
   listResources(): void {
     this.resourcesService.getAll().subscribe(
