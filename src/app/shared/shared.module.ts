def9a0173440f96271db908bfb55b14954c06890
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-12 18:17:54 -0300

    Organizado as importações dos componentes angular material.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+
+// componentes do angular/material
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material';
@@ -10,22 +12,36 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { FormFieldErrorComponent } from './components/form-field-error/form-field-error.component';
 import { ServerErrorMessagesComponent } from './components/server-error-messages/server-error-messages.component';
 import { ButtonsGroupComponent } from './components/buttons-group/buttons-group.component';
-import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
-import { MatTooltipModule } from '@angular/material/tooltip';
 
 // componentes do primeng
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { PanelModule } from 'primeng/panel';
 import { CardModule } from 'primeng/card';
+
+const material = [
+  MatSidenavModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule,
+  MatSortModule,
+  MatPaginatorModule,
+  MatTableModule,
+  MatTooltipModule,
+]
 
 
 @NgModule({
@@ -37,18 +53,8 @@ import { CardModule } from 'primeng/card';
     InputTextModule,
     PanelModule,
     CardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
     FormsModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatTableModule,
-    MatTooltipModule,
+    material
   ],
   declarations: [
     BreadCrumbComponent,
@@ -70,18 +76,8 @@ import { CardModule } from 'primeng/card';
     InputTextModule,
     PanelModule,
     CardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
     FormsModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatTableModule,
-    MatTooltipModule,
+    material
   ]
 })
 export class SharedModule { }
