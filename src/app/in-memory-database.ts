d7cae96a7ddfd1692a208dc74ca430383892fa34
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-12 21:18:36 -0300

    Inclusão de registros fakes Driver para popular tabela do formulário de listagem.

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -8,6 +8,16 @@ export class InMemoryDatabase implements InMemoryDbService {
     let drivers: Driver[] = [
       { id: 1, name: 'João Paulo', dateOfBirth: new Date(1992, 9, 24), cnh: '12345632112', cpf: '12345632112', phone: '62984994081' },
       { id: 2, name: 'João Pedro', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632112', cpf: '12345632112', phone: '62984994081' },
+      { id: 3, name: 'Raimundo Pereira', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632112', cpf: '12345632112', phone: '62984994081' },
+      { id: 4, name: 'José das Couves', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632112', cpf: '12345632112', phone: '62984994081' },
+      { id: 5, name: 'Eduardo', dateOfBirth: new Date(1990, 3, 9), cnh: '54345632112', cpf: '12345632112', phone: '62984994081' },
+      { id: 6, name: 'Mônica', dateOfBirth: new Date(1970, 7, 9), cnh: '12349082112', cpf: '12345632112', phone: '62984994081' },
+      { id: 7, name: 'Maria', dateOfBirth: new Date(1980, 5, 9), cnh: '12345765112', cpf: '12345632112', phone: '62984994081' },
+      { id: 8, name: 'Paula', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081' },
+      { id: 9, name: 'Vanessa', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081' },
+      { id: 10, name: 'Daniel', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081' },
+      { id: 11, name: 'Moreira Ferreira', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081' },
+      { id: 12, name: 'Joana M', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081' },
     ];
 
 
