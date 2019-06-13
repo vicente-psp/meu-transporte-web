478a16cff86ae63030a0f280ebd7e05407e3e7a7
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-12 21:32:09 -0300

    Criado método deleteResource com confirmação utilizando componente do primeng e criado método para retornar quantidade de itens exibidos na tabela.

--- src/app/pages/drivers/driver-list/driver-list.component.ts
+++ src/app/pages/drivers/driver-list/driver-list.component.ts
@@ -1,24 +1,32 @@
 import { Component } from '@angular/core';
 
+import { ConfirmationService } from 'primeng/api';
+
 import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 
 import { Driver } from '../shared/driver.model';
 import { DriverService } from '../shared/driver.service';
 
+
 @Component({
   selector: 'app-driver-list',
   templateUrl: './driver-list.component.html',
-  styleUrls: ['./driver-list.component.css']
+  styleUrls: ['./driver-list.component.css'],
+  providers: [
+    ConfirmationService
+  ]
 })
 export class DriverListComponent extends BaseResourceListComponent<Driver> {
 
   showFilter: boolean = false;
-
   public displayedColumns = ['name', 'dateOfBirth', 'cnh', 'cpf', 'phone', 'actions'];
 
-  constructor(private driverService: DriverService) {
+
+  constructor(private driverService: DriverService,
+              private confirmationService: ConfirmationService) {
     super(driverService, new Driver());
   }
+  
 
   changeResources(event) {
     this.resources.data = event;
@@ -26,6 +34,26 @@ export class DriverListComponent extends BaseResourceListComponent<Driver> {
 
   setShowFilter(event): void {
     this.showFilter = event;
+  }
+
+  getLength(): number {
+    return this.resources.data.length;
+  }
+
+  deleteResource(obj: Driver) {
+    this.confirmationService.confirm({
+        message: `Deseja remover o motorista ${obj.name}?`,
+        header: 'Excluir registro',
+        icon: 'pi pi-info-circle',
+        acceptLabel: 'Sim',
+        rejectLabel: 'Não',
+        accept: () => {
+          this.driverService.delete(obj.id).subscribe(
+            () => this.listResources(),
+            () => alert('Erro ao remover motorista')
+          )
+        }
+    });
   }
 
 }
