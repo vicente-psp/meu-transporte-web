278c867f3be52c410de1f04fd9347b33cb0fc6cb
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-15 00:41:20 -0300

    Removido imports não utilizado *estão sendo importados pelo módulo SharedModule.

--- src/app/pages/clients/clients.module.ts
+++ src/app/pages/clients/clients.module.ts
@@ -1,40 +1,26 @@
 import { NgModule } from '@angular/core';
 
-import { IMaskModule } from 'angular-imask';
-
 import { SharedModule } from '../../shared/shared.module';
 
 import { ClientsRoutingModule } from './clients-routing.module';
 import { ClientFormComponent } from './client-form/client-form.component';
 import { ClientListComponent } from './client-list/client-list.component';
+import { ClientFilterComponent } from './client-filter/client-filter.component';
+import { ClientDetailDialogComponent } from './client-list/client-detail-dialog/client-detail-dialog.component';
 
-import { MAT_DATE_LOCALE, MAT_DATE_FORMATS } from '@angular/material';
-
-export const DD_MM_YYYY_Format = {
-  parse: {
-      dateInput: 'LL',
-  },
-  display: {
-      dateInput: 'DD/MM/YYYY',
-      monthYearLabel: 'MMM YYYY',
-      dateA11yLabel: 'LL',
-      monthYearA11yLabel: 'MMMM YYYY',
-  },
-};
 
 @NgModule({
   imports: [
     SharedModule,
     ClientsRoutingModule,
-    IMaskModule,
   ],
   declarations: [
     ClientFormComponent,
-    ClientListComponent
+    ClientListComponent,
+    ClientFilterComponent,
+    ClientDetailDialogComponent
   ],
   providers: [
-    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
-    { provide: MAT_DATE_FORMATS, useValue: DD_MM_YYYY_Format }
   ],
 })
 export class ClientsModule { }
