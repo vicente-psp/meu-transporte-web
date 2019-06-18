b495c5fcdacc2e35c416f818cde73126e083af97
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-17 23:02:22 -0300

    Incluído atributos e métodos.

--- src/app/pages/drivers/driver-form/driver-form.component.ts
+++ src/app/pages/drivers/driver-form/driver-form.component.ts
@@ -5,6 +5,7 @@ import { BaseResourceFormComponent } from 'src/app/shared/components/base-resour
 import { Driver } from '../shared/driver.model';
 import { DriverService } from '../shared/driver.service';
 
+
 @Component({
   selector: 'app-driver-form',
   templateUrl: './driver-form.component.html',
@@ -16,8 +17,31 @@ export class DriverFormComponent extends BaseResourceFormComponent<Driver> {
   public cpfMask = {mask: '000.000.000-00'};
   public cnhMask = {mask: '00000000000'};
   public dateMask = {mask: '00/00/0000'};
+  public passwordRepeat = '';
+  public typePassword = true;
+  public typePasswordRepeat = true;
+  public maxDate: Date = new Date();
+  
+  public listCnhCategory = [
+    { value: 'A' },
+    { value: 'B' },
+    { value: 'AB' },
+    { value: 'C' },
+    { value: 'D' },
+    { value: 'E' },
+  ];
 
-  maxDate: Date = new Date();
+  public listGender = [
+    { value: 'MALE', viewValue: 'Masculino' },
+    { value: 'FEMALE', viewValue: 'Feminino' },
+  ];
+
+  public listRole = [
+    { value: 'ROLE_ADMIN', viewValue: 'Administrador' },
+    { value: 'ROLE_CLIENT', viewValue: 'Cliente' },
+    { value: 'ROLE_DRIVER', viewValue: 'Motorista' },
+  ];
+  
 
   constructor(
     protected driverService: DriverService,
@@ -25,6 +49,51 @@ export class DriverFormComponent extends BaseResourceFormComponent<Driver> {
   ) {
     super(injector, new Driver(), driverService, Driver.fromJson);
     this.maxDate = new Date(new Date().getFullYear() - 18, new Date().getMonth() + 1, new Date().getDate());
+    this.resource.role = 'ROLE_DRIVER';
+  }
+
+  
+  isShowErrorMessages(): boolean {
+    if (this.isStringValid(this.resource.password) && this.isStringValid(this.passwordRepeat)) {
+      return this.resource.password !== this.passwordRepeat;
+    }
+    return false;
+  }
+
+  changePassword(): void {
+    this.typePassword = !this.typePassword;
+  }
+
+  passwordMatIcon(): string {
+    return this.typePassword ? 'visibility' : 'visibility_off';
+  }
+
+  changePasswordRepeat(): void {
+    this.typePasswordRepeat = !this.typePasswordRepeat;
+  }
+
+  passwordRepeatMatIcon(): string {
+    return this.typePasswordRepeat ? 'visibility' : 'visibility_off';
+  }
+
+  salvarDriver(): void {
+    if (!this.isStringValid(this.resource.password)) {
+      return;
+    }
+    if (!this.isStringValid(this.passwordRepeat)) {
+      return;
+    }
+    if (this.resource.password !== this.passwordRepeat) {
+      return;
+    }
+    this.resource.cpf = this.removeMask(this.resource.cpf);
+    this.resource.phone = this.removeMask(this.resource.phone);
+    this.salvar();
+  }
+
+  removeMask(textValue: string): string {
+    textValue = textValue.replace(/[^\d]+/g, '');
+    return textValue;
   }
 
   protected isValidResource(): boolean {
@@ -41,6 +110,18 @@ export class DriverFormComponent extends BaseResourceFormComponent<Driver> {
       return false;
     }
     if (!this.isStringValid(this.resource.phone)) {
+      return false;
+    }
+    if (!this.isStringValid(this.resource.gender)) {
+      return false;
+    }
+    if (!this.isStringValid(this.resource.cnhCategory)) {
+      return false;
+    }
+    if (!this.isStringValid(this.resource.userName)) {
+      return false;
+    }
+    if (!this.isStringValid(this.resource.password)) {
       return false;
     }
     return true;
