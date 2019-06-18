9210311b63f5546e024055c74d8a7722220527e0
Vicente Silva <vicente.pspenha@gmail.com>
2019-06-17 22:34:39 -0300

    Alterado atributo id para idUser.

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -6,28 +6,28 @@ import { Product } from './pages/products/shared/product.model';
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
     let drivers: Driver[] = [
-      { id: 1, name: 'João Paulo', dateOfBirth: new Date(1992, 9, 24), cnh: '12345632112', cpf: '12345632112', phone: '62984994081', gender: 'M' },
-      { id: 2, name: 'João Pedro', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632112', cpf: '12345632112', phone: '62984994081', gender: 'M' },
-      { id: 3, name: 'Raimundo Pereira', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632112', cpf: '12345632112', phone: '62984994081', gender: 'M' },
-      { id: 4, name: 'José das Couves', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632112', cpf: '12345632112', phone: '62984994081', gender: 'M' },
-      { id: 5, name: 'Eduardo', dateOfBirth: new Date(1990, 3, 9), cnh: '54345632112', cpf: '12345632112', phone: '62984994081', gender: 'M' },
-      { id: 6, name: 'Mônica', dateOfBirth: new Date(1970, 7, 9), cnh: '12349082112', cpf: '12345632112', phone: '62984994081', gender: 'F' },
-      { id: 7, name: 'Maria', dateOfBirth: new Date(1980, 5, 9), cnh: '12345765112', cpf: '12345632112', phone: '62984994081', gender: 'F' },
-      { id: 8, name: 'Paula', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081', gender: 'F' },
-      { id: 9, name: 'Vanessa', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081', gender: 'F' },
-      { id: 10, name: 'Daniel', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081', gender: 'M' },
-      { id: 11, name: 'Moreira Ferreira', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081', gender: 'M' },
-      { id: 12, name: 'Joana M', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081', gender: 'F' },
+      { idUser: 1, name: 'João Paulo', dateOfBirth: new Date(1992, 9, 24), cnh: '12345632112', cpf: '12345632112', phone: '62984994081', gender: 'M' },
+      { idUser: 2, name: 'João Pedro', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632112', cpf: '12345632112', phone: '62984994081', gender: 'M' },
+      { idUser: 3, name: 'Raimundo Pereira', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632112', cpf: '12345632112', phone: '62984994081', gender: 'M' },
+      { idUser: 4, name: 'José das Couves', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632112', cpf: '12345632112', phone: '62984994081', gender: 'M' },
+      { idUser: 5, name: 'Eduardo', dateOfBirth: new Date(1990, 3, 9), cnh: '54345632112', cpf: '12345632112', phone: '62984994081', gender: 'M' },
+      { idUser: 6, name: 'Mônica', dateOfBirth: new Date(1970, 7, 9), cnh: '12349082112', cpf: '12345632112', phone: '62984994081', gender: 'F' },
+      { idUser: 7, name: 'Maria', dateOfBirth: new Date(1980, 5, 9), cnh: '12345765112', cpf: '12345632112', phone: '62984994081', gender: 'F' },
+      { idUser: 8, name: 'Paula', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081', gender: 'F' },
+      { idUser: 9, name: 'Vanessa', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081', gender: 'F' },
+      { idUser: 10, name: 'Daniel', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081', gender: 'M' },
+      { idUser: 11, name: 'Moreira Ferreira', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081', gender: 'M' },
+      { idUser: 12, name: 'Joana M', dateOfBirth: new Date(1990, 3, 9), cnh: '12345632908', cpf: '12345632112', phone: '62984994081', gender: 'F' },
     ];
 
 
     let clients: Client[] = [
-      { id: 1, socialName: 'PANIFICADORA X LTDA', nameFantasy: 'PÃO X', cnpj: '12345632112', phone: '62984496473' },
+      { idUser: 1, socialName: 'PANIFICADORA X LTDA', nameFantasy: 'PÃO X', cnpj: '13424945000182', phone: '62984496473' },
     ];
 
 
     let products: Product[] = [
-      { id: 1, description: 'Coca-cola', unitValue: 2.45 },
+      { idUser: 1, description: 'Coca-cola', unitValue: 2.45 },
     ];
 
     return {drivers, clients, products};
